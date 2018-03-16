5da84a4b852e06fd13546af9dc53f2d0f9453718
Rani Shah <rani76@gmail.com>
2018-03-16 21:47:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,8 @@ class Triangle
       @z = z
    end
 
-   def kind
-     self.validate 
+def kind
+  if x > 0 && y > 0 && z > 0
     if x == y && y == x && x == z
         :equilateral
       elsif (x == y || y == z || x == z) && !(x == y && y = z && x == z)
